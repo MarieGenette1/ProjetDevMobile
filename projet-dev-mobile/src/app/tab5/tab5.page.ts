f6437087bfad2cfe7119e3624d0d13926b18cd42
genette2u <marie.genette.rnd@gmail.com>
2021-05-21 10:19:31 +0200

    modifications tab5

--- projet-dev-mobile/src/app/tab5/tab5.page.ts
+++ projet-dev-mobile/src/app/tab5/tab5.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-tab5',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab5.page.scss'],
 })
 export class Tab5Page implements OnInit {
-
+  private trueFormControl = new FormControl(false);
+  private falseFormControl = new FormControl(false);
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSearchInput($event: any) {
+  }
+  onSearchCancel($event: any) {
+  }
 }
