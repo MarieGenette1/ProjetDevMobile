0698dda426b858fd11981b26725a6774e7665bcf
EricHoffmann57 <81462264+EricHoffmann57@users.noreply.github.com>
2021-05-23 19:28:47 +0200

    Update tab3.page.ts

--- projet-dev-mobile/src/app/tab3/tab3.page.ts
+++ projet-dev-mobile/src/app/tab3/tab3.page.ts
@@ -8,8 +8,8 @@ import { Component } from '@angular/core';
 export class Tab3Page {
 
   constructor() {}
-
-  onSearchInput() {}
-
-  onSearchCancel() {}
+ onSearchInput($event: any) {
+  }
+  onSearchCancel($event: any) {
+  }
 }
