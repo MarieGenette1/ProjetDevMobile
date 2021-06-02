de303d516b2df5f051dea9f0b4ccd27d11b3bb3c
EricHoffmann57 <81462264+EricHoffmann57@users.noreply.github.com>
2021-06-02 18:46:38 +0200

    Update formulaire Tab5 Création dossier "detail" pour afficher les informations complémentaires d'une personne avec routing entre tab1 et detail. Le dossier service contient un fichier user.service.ts qui sert à lier cees deux pages et teste le fonctionnement optimal.

--- projet-dev-mobile/src/app/tab1/tab1.page.ts
+++ projet-dev-mobile/src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {ApiService} from '../api.service';
+import {Router} from '@angular/router';
+import {UserService} from '../service/user.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,14 +9,12 @@ import {ApiService} from '../api.service';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  users: any[];
+  numberContact: any[];
 
-  users : any[];
-  numberContact : any[];
-
-  constructor(public _apiService : ApiService) {
+  constructor(public apiService: ApiService, public router: Router, public userService: UserService) {
     this.getUser();
     this.getNumberUser();
-
   }
 
  onSearchInput($event: any) {
@@ -23,23 +23,22 @@ export class Tab1Page {
   }
 
   getUser(){
-    this._apiService.getUser().subscribe((res:any) =>{
-      console.log("SUCCESS GET USER ===", res);
+    this.apiService.getUser().subscribe((res: any) =>{
+      console.log('SUCCESS GET USER ===', res);
       this.users = res;
     }, (error: any) =>{
-      console.log("ERRRO GET USER ===", error);
-    }) 
+      console.log('ERRRO GET USER ===', error);
+    });
 
   }
 
   getNumberUser(){
-    this._apiService.getNumberUser().subscribe((res:any) =>{
-      console.log("SUCCESS GET NUMBER ===", res);
+    this.apiService.getNumberUser().subscribe((res: any) =>{
+      console.log('SUCCESS GET NUMBER ===', res);
       this.numberContact = res;
     }, (error: any) =>{
-      console.log("ERRRO GET USER ===", error);
-    }) 
+      console.log('ERRRO GET USER ===', error);
+    });
   }
-
 }
 
